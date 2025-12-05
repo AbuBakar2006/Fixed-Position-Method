bcf851b41410dd79574a4405c486d228b3a0488e
AbuBakar2006 <m.abubakarasif2006@gmail.com>
2025-12-05 14:48:08 +0500

    False-Position-Method

---
+++ Fixed-Point-Method.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sys
+
+
+def g(x): return (2-np.e**x+x**2)/3
+
+
+p0 = float(input("Enter p0: "))
+n = 1
+N = 10
+TOL = 1e-2
+print('n         pn')
+while n <= N:
+    p = g(p0)
+    print(f'{n} \
+    {p:.15f} ')
+    if abs(p - p0) <= TOL:
+        print('Fixed-Point has converged')
+        print('final p =', p)
+        sys.exit(0)
+    else:
+        p0 = p
+
+    n += 1
+print('zero not found to the desired tolerance')
